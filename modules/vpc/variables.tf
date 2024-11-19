b5676bf3cebcd499cfe8fdd795e7ba528275c750
suojae <suojae.developer@gmail.com>
2024-11-19 15:45:05 +0900

    feat: vpc 모듈 추가

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,29 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+}
+
+variable "public_subnet_az" {
+  description = "Availability zone for the public subnet"
+  type        = string
+}
+
+variable "private_subnet_az" {
+  description = "Availability zone for the private subnet"
+  type        = string
+}
