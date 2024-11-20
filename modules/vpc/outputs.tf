b42533d4e67fa7e06b133440015752277af6a881
suojae <suojae.developer@gmail.com>
2024-11-20 11:50:05 +0900

    refactor: vpc 모듈에서 서브넷, 라우트테이블 분리

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,19 +1,3 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "public_subnet_id" {
-  value = aws_subnet.public.id
-}
-
-output "private_subnet_id" {
-  value = aws_subnet.private.id
-}
-
-output "public_route_table_id" {
-  value = aws_route_table.public.id
-}
-
-output "private_route_table_id" {
-  value = aws_route_table.private.id
+  value = aws_vpc.this.id
 }
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,29 +1,9 @@
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+variable "cidr_block" {
+  description = "VPC CIDR 블록"
   type        = string
 }
 
-variable "vpc_name" {
-  description = "Name of the VPC"
-  type        = string
-}
-
-variable "public_subnet_cidr" {
-  description = "CIDR block for the public subnet"
-  type        = string
-}
-
-variable "private_subnet_cidr" {
-  description = "CIDR block for the private subnet"
-  type        = string
-}
-
-variable "public_subnet_az" {
-  description = "Availability zone for the public subnet"
-  type        = string
-}
-
-variable "private_subnet_az" {
-  description = "Availability zone for the private subnet"
-  type        = string
+variable "tags" {
+  description = "태그 맵"
+  type        = map(string)
 }
