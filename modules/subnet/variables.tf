35cb5d7b5294b050b088e2389b45c498cda96dd1
suojae <suojae.developer@gmail.com>
2024-11-20 11:51:23 +0900

    feat: 서브넷 추가

---
+++ modules/subnet/variables.tf
@@ -0,0 +1,24 @@
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
+
+variable "cidr_block" {
+  description = "The CIDR block of the subnet"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "The availability zone"
+  type        = string
+}
+
+variable "map_public_ip_on_launch" {
+  description = "Whether to assign a public IP on launch"
+  type        = bool
+}
+
+variable "tags" {
+  description = "A map of tags"
+  type        = map(string)
+}
