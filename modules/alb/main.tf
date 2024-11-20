77fcee49004a05955c559edcdf99b0b588d33f6b
suojae <suojae.developer@gmail.com>
2024-11-20 11:53:52 +0900

    feat: 로드밸런서 추가

---
+++ modules/alb/main.tf
@@ -0,0 +1,28 @@
+resource "aws_lb" "this" {
+  name               = var.name
+  load_balancer_type = "application"
+  subnets            = var.subnets
+  security_groups    = [var.security_group_id]
+
+  tags = var.tags
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+}
+
+resource "aws_lb_target_group" "this" {
+  name     = var.name
+  port     = var.target_port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
---
+++ modules/alb/variables.tf
@@ -0,0 +1,34 @@
+variable "name" {
+  description = "ALB 이름"
+  type        = string
+}
+
+variable "subnets" {
+  description = "서브넷 ID 리스트"
+  type        = list(string)
+}
+
+variable "security_group_id" {
+  description = "보안 그룹 ID"
+  type        = string
+}
+
+variable "certificate_arn" {
+  description = "SSL 인증서 ARN"
+  type        = string
+}
+
+variable "target_port" {
+  description = "타겟 포트"
+  type        = number
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "tags" {
+  description = "태그 맵"
+  type        = map(string)
+}
