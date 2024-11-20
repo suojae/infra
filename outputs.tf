8debd4a88fd25e5fe74cba8916ceac4dbd724e5f
suojae <suojae.developer@gmail.com>
2024-11-20 11:55:52 +0900

    feat: 메인 모듈추가

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,27 @@
-# Consolidate outputs from modules if necessary
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet_id" {
+  value = module.public_subnet.subnet_id
+}
+
+output "private_subnet_id" {
+  value = module.private_subnet.subnet_id
+}
+
+output "alb_dns_name" {
+  value = module.alb.alb_dns_name
+}
+
+output "ecs_cluster_id" {
+  value = module.ecs.ecs_cluster_id
+}
+
+output "rds_endpoint" {
+  value = module.rds.rds_endpoint
+}
+
+output "redis_endpoint" {
+  value = module.redis.redis_endpoint
+}
--- variables.tf
+++ variables.tf
@@ -1,5 +1,11 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
-  type = string
-  default = "ap-northeast-2" // 서울리전
+  type        = string
+  default     = "ap-northeast-2"
+}
+
+variable "db_password" {
+  description = "Database password for RDS"
+  type        = string
+  sensitive   = true
 }
