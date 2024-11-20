baa8942e3d2f0be784504cc43312d7d78d7bf07f
suojae <suojae.developer@gmail.com>
2024-11-20 11:54:41 +0900

    feat: 인터넷 게이트웨이 추가

---
+++ modules/igw/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_id" {
+  description = "VPC ID to attach the Internet Gateway"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
